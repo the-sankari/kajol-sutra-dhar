da9abac09a9902b59f603ea32f8e92aa34b70338
the-sankari <kajol.sd777@gmail.com>
2025-06-07 18:42:21 +0300

    Added more nav tab

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -58,7 +58,9 @@ const Navbar = () => {
             {[
               { label: "Home", to: "/" },
               { label: "About", to: "/about" },
-              { label: "Services", to: "/services" },
+              { label: "Projects", to: "/projects" },
+              { label: "Skills", to: "/skills" },
+              { label: "Blog", to: "/blog" },
               { label: "Contact", to: "/contact" },
             ].map((item) => (
               <li key={item.to}>
